1b3235bf088a254fc1a623662f0b615589eb4331
qazihamza88 <qazihamzaali7@gmail.com>
2024-04-02 15:54:22 +0500

    updated

--- src/component/Ceo.js
+++ src/component/Ceo.js
@@ -20,9 +20,8 @@ function Ceo() {
           <div className="right-container1">
             <h1 style={tws}>Message From CEO</h1>
             <p style={{ fontSize: "21px", marginBottom: "26px" }}>
-              Create a vision for the life you really want and then work relentlessly towards
-              <br />
-              making it a reality.”
+              Create a vision for the life you really want and then work relentlessly <br/>
+              towards making it a reality.”
             </p>
             <p style={{ fontSize: "21px", marginBottom: "26px" }}>
               When I started TWS, I had one goal, to establish it as the top
@@ -30,8 +29,8 @@ function Ceo() {
               are heading towards <br/> achieving all our goals one step at a time. My
               vision has not changed, it has <br/> multiplied into achieving more for
               the company, my employees and the <br/> clients that I work with. With
-              TWS, I aim to help companies that are making a <br/> difference in their
-              respective fields. We nourish new talent and set new <br/> standards of
+              TWS, I aim to help companies that are making <br/>
+              a difference in their respective fields. We nourish new talent and set new <br/> standards of
               excellence by reclaiming our power and position in the world.<br/>
               Through the undertaking of this process we can break free from the
               past and <br/> enter the future with a new and better purpose.
@@ -41,20 +40,18 @@ function Ceo() {
               nation <br/> should master the art of digitization and
               self-entrepreneurship because that <br/> is how we can put Pakistan on
               the map. I have taken it upon myself to further <br/> Pakistan’s role in
-              the global movement of transformational tech. Therefore we<br/> will
-              keep on moving forward with sheer hard work, determination and <br/>
+              the global movement of transformational tech.Therefore we
+              will keep on moving forward with sheer hard work, determination and <br/>
               perseverance, and only then can we realize all our dreams!
             </p>
             <div>
-            {/* <p style={{ fontSize: "21px", marginBottom: "30px" ,color:"#007EC5" }}>
-                </p> */}
                 <div>
           <div style={{ textAlign: "center", padding: "15px 0 60px" }}>
             <h4 style={{fontFamily:"Open Sans,sans-serif",color:"#3e4a59"}}>Muhammad Adeel</h4>
             <h5 style={{fontWeight:"500", color:"#9b9292",fontSize:"15px",
             fontFamily:'Open Sans,sans-serif'}}>Founder & CEO</h5>
             <a href="https://www.linkedin.com/in/muhammad-adeel-863828108/"
-              aria-label="Linkedin" target="_blank"             >
+              aria-label="Linkedin" target="_blank" rel="noreferrer" >
               <FaLinkedin
                 style={{
                   width: "20px",
